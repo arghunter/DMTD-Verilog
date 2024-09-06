b039c6888bd01d3195e3ca185d0061f3a22c6683
Armaan Gomes <91099806+arghunter@users.noreply.github.com>
2024-09-06 01:55:39 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 // This project was a collaboration between me(arghunter) and the other collaborators on this repository (Acknowledged in the ReadMe)
-module tt_um_i2s_to_pwm_arghunter (
+module tt_um_dmtd_arghunter (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
